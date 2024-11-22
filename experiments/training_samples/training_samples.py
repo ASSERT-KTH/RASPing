819b5d85c9dab85c87804454ad9a51f68587a390
André Silva <andre15andre@hotmail.com>
2024-11-22 10:03:01 +0100

    update experiment

--- experiments/training_samples/training_samples.py
+++ experiments/training_samples/training_samples.py
@@ -9,10 +9,11 @@ if module_path not in sys.path:
 
 from src.functions import *
 
-from src.loss import cross_entropy_loss, cross_entropy_loss_smoothed_accuracy, cross_entropy_loss_with_perfect_sequence
+from src.loss import cross_entropy_loss
 
-def test_loss_function(model_name: str, loss_fn, max_len: int = 10, n_samples: int = 5000, n_epochs: int = 50000, batch_size: int = 256, learning_rate: float = 1e-04, output_dir: str = None):
+def test_n_samples_function(model_name: str, n_samples, max_len: int = 10, n_epochs: int = 50000, batch_size: int = 256, learning_rate: float = 1e-04, output_dir: str = None):
     model = generateModel(model_name, max_len)
+    model.setRandomWeights()
     X, Y = generateAndEncodeData(model_name, max_len, n_samples, removeDuplicates=True)
 
     X_train = X[:int(0.85*len(X))]
@@ -20,27 +21,18 @@ def test_loss_function(model_name: str, loss_fn, max_len: int = 10, n_samples: i
     X_val = X[int(0.85*len(X)):]
     Y_val = Y[int(0.85*len(Y)):]
 
-    return model.train(X_train,Y_train, n_epochs, batch_size, learning_rate, False, X_val, Y_val, 0, 10, loss_fn=loss_fn, output_dir=output_dir)
+    return model.train(X_train,Y_train, n_epochs, batch_size, learning_rate, False, X_val, Y_val, 0, 10, loss_fn=cross_entropy_loss, output_dir=output_dir)
 
 @click.command()
 @click.option("--model_name", type=str, help="The name of the program/model to train")
-@click.option("--loss_fn_name", type=str, help="The name of the loss function to test")
-def run_test(model_name, loss_fn_name):
+@click.option("--n_samples", type=int, help="The number of samples to generate for training/validation")
+def run_test(model_name, n_samples):
     if model_name not in getAcceptedNamesAndInput().keys():
         raise ValueError(f"Model {model_name} not found")
 
-    loss_fns = {
-        "cross_entropy_loss": cross_entropy_loss,
-        "cross_entropy_loss_smoothed_accuracy": cross_entropy_loss_smoothed_accuracy,
-        "cross_entropy_loss_with_perfect_sequence": cross_entropy_loss_with_perfect_sequence
-    }
-    if loss_fn_name not in loss_fns:
-        raise ValueError(f"Loss function {loss_fn_name} not found")
-    loss_fn = loss_fns[loss_fn_name]
-
-    print(f"Testing {model_name} with {loss_fn_name}...")
-    metrics, validation = test_loss_function(model_name, loss_fn, output_dir=f"saved_data/{model_name}/{loss_fn_name}/")
-    return model_name, loss_fn_name, metrics, validation
+    print(f"Testing {model_name} with {n_samples} samples...")
+    metrics, validation = test_n_samples_function(model_name, n_samples, output_dir=f"saved_data/{model_name}/{n_samples}/")
+    return model_name, n_samples, metrics, validation
 
 if __name__ == "__main__":
     run_test()
