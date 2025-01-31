e8a6c23a40a9b61487ddf74fc71a5be5180e93c1
André Silva <andre15andre@hotmail.com>
2025-01-31 18:51:40 +0100

    fix numpy encoder

--- src/jsonl.py
+++ src/jsonl.py
@@ -17,6 +17,8 @@ class NumpyJSONEncoder(json.JSONEncoder):
             return float(obj)
         if isinstance(obj, np.ndarray):
             return obj.tolist()
+        if isinstance(obj, np.bool_):
+            return bool(obj)
         return super().default(obj)
 
 def stream_jsonl(filename: str) -> Iterable[Dict]:
