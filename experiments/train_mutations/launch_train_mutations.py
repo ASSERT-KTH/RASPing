fa0d841be3ed17e17ebdfe70f540db05d04dd580
André Silva <andre15andre@hotmail.com>
2025-05-10 16:18:09 +0200

    only run cross entropy

--- experiments/train_mutations/launch_train_mutations.py
+++ experiments/train_mutations/launch_train_mutations.py
@@ -110,8 +110,6 @@ def main():
     # List of loss functions to test
     loss_functions = [
         "cross_entropy_loss",
-        "cross_entropy_loss_smoothed_accuracy",
-        "cross_entropy_loss_with_perfect_sequence",
     ]
 
     for _, row in df.iterrows():
